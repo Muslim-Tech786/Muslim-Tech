be8575975d1e8e5d796d956829c661353606ebd1
Muslim Tech Team <124895681+Muslim-Tech786@users.noreply.github.com>
2024-05-06 07:19:43 +0430

    Add files via upload

--- M-TECH.py
+++ M-TECH.py
@@ -1,3 +1,5 @@
-from SS import muslim
-
-muslim()
+import os,platform
+platForm=platform.machine()
+if '64' in platForm:import main64
+elif '32' in platForm:import main32
+else:print('Your device can\'t support our tool');exit()
