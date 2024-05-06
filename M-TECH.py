70272b690b880387b0377107460c4dd9e9c6cc99
Muslim Tech Team <124895681+Muslim-Tech786@users.noreply.github.com>
2024-05-06 11:09:12 +0430

    Update M-TECH.py

--- M-TECH.py
+++ M-TECH.py
@@ -1,5 +1,5 @@
 import os,platform
 platForm=platform.machine()
 if '64' in platForm:import main64
-elif '32' in platForm:import main32
+elif '32' in platForm:print('wait for 32bit update')
 else:print('Your device can\'t support our tool');exit()
